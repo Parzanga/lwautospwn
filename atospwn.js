38d74548e17d1b33f7eac48f1ad8c6101dca0db3
Parzanga <54054900+Parzanga@users.noreply.github.com>
2021-03-07 17:13:07 +0400

    Update atospwn.js

--- atospwn.js
+++ atospwn.js
@@ -5,7 +5,6 @@
 // @description  самая конченная залупа которую можно было придумать
 // @author Murka
 // @downloadURL https://github.com/Parzanga/lwautospwn/raw/main/atospwn.js
-// @updateURL https://github.com/Parzanga/lwautospwn/raw/main/atospwn.js
 // @match *://lostworld.io/*
 // @match http://149.28.108.169:4444/
 // @icon http://lostworld.io/img/icon/logo.png
